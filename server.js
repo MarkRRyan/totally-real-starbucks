b7d93390ca84dbe02715bd2d4f0c1384e706baf0
Mark Ryan <markrryan99@gmail.com>
2024-01-31 21:45:00 -0800

    adding controllers, hooking up mongodb

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
-
+// dependencies
 const express = require('express');
+const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI
 const PORT = process.env.PORT;
+const menuController = require('./controllers/menu.js')
+
+// mongo connection
+mongoose.connect(mongoURI);
+
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + 'error with mongo connection'))
+db.on('connected', () => console.log('mongoDB is petting some mongoose!'))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
 const app = express();
+app.use('/menu', menuController)
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.get('/menu', (req, res) => {
+    res.render('menu.ejs')
 })
 
 app.listen(PORT, () => {
